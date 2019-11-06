84fadf563fd791b7fc9396be8db418abbbf6ff21
vladhiriscau <vlad.hiriscau@yahoo.com>
2019-11-06 20:57:07 +0200

    added message to test commiting a new version

--- HelloWord.java
+++ HelloWord.java
@@ -2,6 +2,7 @@ public class HelloWorld {
 
         public static void main(String[] args) {	
                 System.out.println("Hellow World!");
+System.out.println("testing a new version");
  
        }	
 }
